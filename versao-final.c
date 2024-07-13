5f82cbd25d942e3241e21217da1b5ac4842e11ab
Vinicius Abaddy <vinicius_abaddy@hotmail.com>
2024-07-13 16:10:08 -0300

    versao final zoo

--- versao-final.c
+++ versao-final.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 	typedef struct{	//struct anonima com o "apelido" de central_zoo;
 
@@ -32,10 +33,31 @@
     	puts("5 - Sair");
 
     	scanf("%d", &menu);
+    	
+    	system("cls");
 
     	return menu; //retorna a opcao do usuario;
 	}
 
+	void imprimir_dados(central_zoo animais[], int total_cadastrado){ //funcao para imprimir o relatorio;
+		
+		for(int i=0; i<total_cadastrado; i++){ //laco que imprime os dados de todos os animais;
+
+			printf("Raca: %s\n", animais[i].raca);
+
+			printf("Sexo: %s\n", animais[i].sexo);
+
+			printf("Idade: %d\n", animais[i].idade);
+
+			printf("codigo: %.3d\n", animais[i].codigo);
+
+			printf("Data de entrada: %s", animais[i].data_entrada);
+
+			printf("\n\n");
+			
+		}
+	}
+	
 	void verifica_codigo(central_zoo animais[], int total_cadastrado){ //funcao para verificar se novos codigos ja existem no sistema;
     																   //caso existam, solicita um novo codigo valido;
 		int i, j; 
@@ -57,40 +79,44 @@
 
 	int cadastro_zoo(central_zoo animais[], int total_cadastrado){ //funcao para cadastrar novos animais;
 
-    	int i = 0, escolha_sexo = 0;
-
-		char continua_cadastro[4]; //string que serve como condicao de parada para do-while;
+    	int i = total_cadastrado, escolha_sexo = 0, continua_cadastro;
 
     	do{ //laco para cadastrar n animais;
-
+			
         	printf("Insira a raca:\n");
         	fflush(stdin);
         	gets(animais[i].raca);
 
-        	printf("Insira o sexo 1-macho ou 2-femea:\n");
-        	scanf("%d", &escolha_sexo);
-        	
-        	if(escolha_sexo == 1){
-        	
-				strcpy(animais[i].sexo, "Macho");
-    		
-			} 
-			
-			else if(escolha_sexo == 2){
+			while(escolha_sexo != 1 || escolha_sexo != 2){
 				
-        		strcpy(animais[i].sexo, "Femea");
-    		} 
-			
-			else{
-				
-        		printf("Escolha invalida.\n");
+        		printf("Insira o sexo 1-macho ou 2-femea:\n");
+        		scanf("%d", &escolha_sexo);
+        	
+        		if(escolha_sexo == 1){
+        	
+					strcpy(animais[i].sexo, "Macho");
+					break;
     		
+				} 
+			
+				else if(escolha_sexo == 2){
+				
+        			strcpy(animais[i].sexo, "Femea");
+        			break;
+    			} 
+			
+				else{
+				
+        			printf("Escolha invalida.\n");
+    		
+				}
+			
 			}
-
-        	printf("Insira a idade:\n");
+			
+			printf("Insira a idade: (0-120)\n");
         	scanf("%d", &animais[i].idade);
-
-        	printf("Insira o codigo de registro (exemplo: 777):\n");
+			
+        	printf("Insira o codigo de registro (exemplo: 007):\n");
         	scanf("%d", &animais[i].codigo);
 
         	total_cadastrado++; //incrementa a quantidade de animais cadastrados;
@@ -103,49 +129,60 @@
 
         	i++; //incrementa i para um novo cadastro;
 
-        	printf("Deseja fazer outro cadastro? sim / nao\n"); //interacao com o usuario;
-        	fflush(stdin);
-        	gets(continua_cadastro);
-
-
-   		}while (strcmp(continua_cadastro, "sim") == 0); //condicao de parada do laco;
+        	printf("Deseja fazer outro cadastro? 1-sim / 2-nao\n"); //interacao com o usuario;
+        	scanf("%d", &continua_cadastro);
+        	
+        	system("cls");
+        	
+   		}while(continua_cadastro != 2); //condicao de parada do laco;
 
    		return total_cadastrado; //retorna o numero total de animais cadastrados;
 	}	
 
-	int submenu_pesquisa(int escolha_pesquisa){ //funcao que administra escolha de pesquisa do usuario
+	int submenu_pesquisa(int escolha_pesquisa, char pesquisa_raca[]){ //funcao que administra escolha de pesquisa do usuario
 
 		int sub_escolha;
 
 		printf("Escolha a o tipo de pesquisa que deseja realiza: 1-por sexo ou 2-por raca \n");
 		scanf("%d", &escolha_pesquisa);
+		
+		system("cls");
 
 		if(escolha_pesquisa == 1){
 
 			printf("escolha o genero: 1-macho ou 2-femea \n");
 			scanf("%d", &sub_escolha);
+			
+			system("cls");
 
 			escolha_pesquisa = sub_escolha;
 
 			return escolha_pesquisa;			
 		}
 
-		if(escolha_pesquisa == 2){
-
+		else if(escolha_pesquisa == 2){
+			
+			puts("Digite a raca desejada:");
+			fflush(stdin);
+			gets(pesquisa_raca);
+			
+			system("cls");
+			
 			escolha_pesquisa = 3;
 
 			return escolha_pesquisa;
-
+			return pesquisa_raca[30];
+			
 		}
 	}
 
 	void funcao_pesquisa(central_zoo animais[], int total_cadastrado){ //funcao de pesquisa por sexo ou raca
 
 		int escolha_pesquisa = 0;
-
+		
 		char pesquisa_raca[30];
 
-		escolha_pesquisa = submenu_pesquisa(escolha_pesquisa);
+		escolha_pesquisa = submenu_pesquisa(escolha_pesquisa, pesquisa_raca);
 
 		switch(escolha_pesquisa){
 
@@ -168,11 +205,13 @@
 						printf("\n\n");
 					}
 				}
-
+				
+				system("pause");
+				system("cls");
 				break;
-
+			
 			case 2:
-
+				
 				for(int i=0; i<total_cadastrado;i++){
 
 					if(strcmp(animais[i].sexo, "Femea") == 0){
@@ -190,14 +229,12 @@
 						printf("\n\n");
 					}
 				}
-
+				
+				system("pause");
+				system("cls");
 				break;
 
 			case 3:
-
-				puts("Digite a raca desejada:");
-				fflush(stdin);
-				gets(pesquisa_raca);
 
 				for(int i=0; i<total_cadastrado;i++){
 
@@ -216,7 +253,9 @@
 						printf("\n\n");
 					}
 				}
-
+				
+				system("pause");
+				system("cls");
 				break;
 		}		
 	}
@@ -225,56 +264,47 @@
 
 		printf("O zoologico possui um total de %d animais cadastrados no sistema:\n\n", total_cadastrado);
 
-		for(int i=0; i<total_cadastrado; i++){ //laco que imprime os dados de todos os animais;
-
-			printf("Raca: %s\n", animais[i].raca);
-
-			printf("Sexo: %s\n", animais[i].sexo);
-
-			printf("Idade: %d\n", animais[i].idade);
-
-			printf("codigo: %.3d\n", animais[i].codigo);
-
-			printf("Data de entrada: %s", animais[i].data_entrada);
-
-			printf("\n\n");
-		}
-
+		imprimir_dados(animais, total_cadastrado);
+		
+		system("pause");
+		system("cls");
+		
 	}
 
-	int submenu_animais_por_sexo(){ //funcao para administrar a escolha do usuario
+	int submenu_animais_por_sexo(int op){ //funcao para administrar a escolha do usuario
+		
 
-
-
-	}
-
-	void funcao_animais_sexo(){ //listagem de animais 
-
-
-	}
-
-	void pesquisa_sexo(central_zoo animais[], int total_cadastrado)  {
-		char sexo[20];
-		int op, i, contM = 0, contF = 0; 
-		float SomaM = 0, SomaF = 0, Md;
-
-		printf("\nEscolha uma das opcoes \n");
-		printf("\n1 - Apresentar total de animais por sexo \n");
+		printf("\nEscolha uma das opcoes:\n");
+		
+		printf("1 - Apresentar total de animais por sexo \n");
+		
 		printf("2 - Apresentar a media de idade de animais por sexo \n");
-		printf("Sua escolha: ");
+		
+		printf("Sua escolha: \n "); //interage com o usuario
 		scanf("%d", &op);
+		
+		system("cls");
+		
+		return op; //retorna a escolha;
 
+	}
 
-		if(op == 1) {
+	void funcao_animais_por_sexo(central_zoo animais[], int total_cadastrado){ //faz a media das idades e o numero por sexo de animais
+		
+		int i, contM = 0, contF = 0, SomaM = 0, SomaF = 0, Md = 0;
+		
+		int op = submenu_animais_por_sexo(op); //abre o submenu da funcao;
+
+		if(op == 1) { //se retorna 1 imprime o total de cada animal;
 
 			for(i = 0; i < total_cadastrado; i++)	{
 
-				if(strcmp(animais[i].sexo, "macho") == 0) {
+				if(strcmp(animais[i].sexo, "Macho") == 0) {
 
 					contM++;
 				}
 
-				if(strcmp(animais[i].sexo, "femea") == 0) {
+				if(strcmp(animais[i].sexo, "Femea") == 0) {
 
 					contF++;
 				}
@@ -282,34 +312,52 @@
 
 			printf("\nTotal de machos: %d \n", contM);
 			printf("\nTotal de femeas: %d \n", contF);
+			
+			printf("\n");
+			
+			system("pause");
+			system("cls");
 		}
 
-		if(op == 2) {
+		else if(op == 2){ //se retorna 2 imprime a media das suas idades
 
 			for(i = 0; i < total_cadastrado; i++) {
 
-				if(strcmp(animais[i].sexo, "macho") == 0) {
+				if(strcmp(animais[i].sexo, "Macho") == 0) {
 					contM++;
 					SomaM = SomaM + animais[i].idade;
 				}
 
-				if(strcmp(animais[i].sexo, "femea") == 0) {
+				else if(strcmp(animais[i].sexo, "Femea") == 0) {
 					contF++;
 					SomaF = SomaF + animais[i].idade;
 				}
+				
 			}
-
-			printf("\nMedia de idades dos machos: %.2f", Md = SomaM/contM);
-			printf("\nMedia de idades das femeas: %.2f", Md = SomaF/contF);
-			printf("\n");
+			
+			if(total_cadastrado > 0){
+			
+			printf("\nMedia de idades dos machos: %d", Md = SomaM/contM);
+			printf("\nMedia de idades das femeas: %d", Md = SomaF/contF);
+			printf("\n\n");
+			
+			}
+			else{
+				
+				printf("nenhum animal cadastrado!\n\n");
+			}
+			
+			system("pause");
+			system("cls");	
 		}
+		
 	}
-
+		
 	int main(){ //main do programa
 
 		int total_cadastrado = 0, menu = 0;
 
-		central_zoo animais[1000];
+		central_zoo animais[12000]; //numero baseado no maior zoologico do mundo;
 
     	while(menu != 5){ //laco que permite o usuario interragir com o programa n vezes;
 
@@ -333,9 +381,8 @@
                 	break;
 
             	case 4:
-
-					puts("Listagem de animais por sexo selecionada.");
-					pesquisa_sexo(animais, total_cadastrado);
+            		
+					funcao_animais_por_sexo(animais, total_cadastrado);
                 	break;
 
             	case 5:
